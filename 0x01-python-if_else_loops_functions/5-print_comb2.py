531ab2ad0a9e125532d2d997a835c474bf9ab281
IwegbuJeddy <ijeddy5.0@gmail.com>
2022-09-20 12:13:42 -0700

    Prints 0 - 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    if(i < 10):
-        print("0" + str(i) + ",")
-    elif(i == 99):
-        print(i)
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
     else:
-        print(str(i) + ",")
+        print("{:02}".format(number), end=", ")

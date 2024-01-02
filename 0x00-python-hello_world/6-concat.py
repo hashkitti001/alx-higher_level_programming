185754a975bb80f27c7609dfc7e966c2ab00a7eb
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-02 19:52:34 +0100

    Hot fix to 6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-space = " "
-# Just a comment
-print(f"Welcome to {str1 + space + str2}!")
+str1 += " " + str2
+print("Welcome to {}!".format(str1))

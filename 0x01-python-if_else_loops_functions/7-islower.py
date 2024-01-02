27907c1a66263e89cfa8bc0bbc8ee5e8f8824fa9
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-02 20:29:35 +0100

    Hot fix to task 5 of 0x01-if/else

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def is_lower(c):
+"""A function that checks for lowercase characters"""
+def islower(c):
     if (ord(c) >= 97 and ord(c) <= 122):
         return True
     return False

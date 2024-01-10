aa19e81dfa388700a09252153f1ef53f6d87f942
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-10 12:56:03 +0100

    Modified 2-uniq-add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    def uniq_add(my_list=[]):
-        sum = 0
-        uniq_list = set(my_list)
-        for elem in uniq_list:
-            sum += elem
-        return sum
+def uniq_add(my_list=[]):
+    uniq_list = set(my_list)
+    sum = 0
+    for elem in uniq_list:
+        sum += elem
+    return sum

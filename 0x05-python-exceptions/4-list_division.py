935a046cb96c1372051c7737f1c76b06822ed337
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-22 14:06:04 +0100

    Added submissions for 0x05-Exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,4 +1,4 @@
-def list_quotientision(my_list_1, my_list_2, list_length):
+def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(0, list_length):
         try:

d88571eab4473f55e6ad48b21732142a40d543ea
hashkitti001 <iJeddy5.0@gmail.com>
2024-02-08 17:34:16 +0100

    added area method to rectangle.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,12 +3,15 @@
 class Base:
     """
     Defines a class Base which would be the parent class for other classes in the directory
+    Private variables:
+    __nb_objects(int): The number of objects created from the Base class
     Args: 
-         id(int): 
+         id(int): Identifier of the object instance
     """
     __nb_objects = 0
     def __init__(self, id=None):
-        if self.id is not None:
+        """Initializes a new Base"""
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,70 +1,106 @@
 #!/usr/bin/python3
-from models.base import Base
+from base import Base
+
+
 class Rectangle(Base):
     """Represents a rectangle"""
-    def __init__(self, width, height, x=0, y=0):
+
+    def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Constructor function to the Rectangle class
+        Constructor function for the Rectangle class
 
         Args: 
-          __width(int):  represents the width of the Rectangle instance
-          __height(int): represents the height of the Rectangle instance
-          x(int): Represents the positional x-coordinate of the Rectangle instance
-          y(int): Represents the positional y-coordinate of the Rectangle instance
-          id(int): Identifier of the Rectangle
+          width (int): represents the width of the Rectangle instance
+          height (int): represents the height of the Rectangle instance
+          x (int): Represents the positional x-coordinate of the Rectangle instance
+          y (int): Represents the positional y-coordinate of the Rectangle instance
+          id (int): Identifier of the Rectangle
+          Raises: 
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
         """
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        super().__init(id)
-        
-        @property
-        def get_width(self):
-            """Gets the width of the rectangle"""
-            return self.__width
-        @width.setter
-        def set_width(self, value):
-            """Sets the width of the rectangle"""
-            if(value is not isinstance(int)):
-                raise TypeError("width must be an integer")
-            if(value <= 0):
-               raise ValueError("width must be > 0")
-            self.width = value
-        @property
-        def get_height(self):
-            """Gets the height of the rectangle"""
-            return self.__height
-        
-        @width.setter
-        def set_height(self, value):
-            """Sets the width of the rectangle"""
-            if(value is not isinstance(int)):
-                raise Exception("height must be an integer")
-            if(value <= 0):
-               raise Exception("height must be > 0")
-            self.height = value
-        @property
-        def get_x(self):
-            """Gets the positional x coordinate of the rectangle"""
-            return self.__x
-        @x.setter
-        def set_x(self, value):
-            """Sets the positional x coordinate to a value of 'value'"""
-            if(value is not isinstance(int)):
-                raise Exception("x must be an integer")
-            if(value <= 0):
-               raise Exception("x must be > 0")
-            self.x = value
-        @property
-        def get_y(self):
-            """Gets the positional y coordinate of the rectangle"""
-            return self.__y
-        @x.setter
-        def set_y(self, value):
-            """Sets the positional y coordinate to a value of 'value'"""
-            if(value is not isinstance(int)):
-                raise Exception("x must be an integer")
-            if(value <= 0):
-               raise Exception("x must be > 0")
-            self.height = value
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Gets the width of the rectangle"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Gets the height of the rectangle"""
+        return self.__height
+
+    @property
+    def x(self):
+        """Gets the positional x coordinate of the rectangle"""
+        return self.__x
+
+    @property
+    def y(self):
+        """Gets the positional y coordinate of the rectangle"""
+        return self.__y
+
+    @property
+    def width(self):
+        """Gets the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def set_width(self, value):
+        """Sets the width of the rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if (value <= 0):
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def set_height(self, value):
+        """Sets the width of the rectangle"""
+        if type(value) != int:
+            raise Exception("height must be an integer")
+        if (value <= 0):
+            raise Exception("height must be > 0")
+        self.__height = value
+    
+    @property
+    def x(self):
+        """Gets the positional x coordinate of the rectangle"""
+        return self.__x
+
+    @x.setter
+    def set_x(self, value):
+        """Sets the positional x coordinate to a value of 'value'"""
+        if type(value) != int:
+            raise Exception("x must be an integer")
+        if (value <= 0):
+            raise Exception("x must be > 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Gets the positional y coordinate of the rectangle"""
+        return self.__y
+
+    @y.setter
+    def set_y(self, value):
+        """Sets the positional y coordinate to a value of 'value'"""
+        if type(value) != int:
+            raise Exception("y must be an integer")
+        if (value <= 0):
+            raise Exception("y must be > 0")
+        self.__y = value
+    def area(self):
+        return self.width * self.height

7bb6aff378a332148871c06c86753ee16d24ad58
hashkitti001 <iJeddy5.0@gmail.com>
2024-04-08 23:15:16 +0100

    rewrite to make 2-arguments conform to tests

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const cnt = process.argv.length;
-if (cnt <= 2) {
-  console.log('No argument');
-} else if (cnt === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+console.log(cnt <= 2 ? 'No argument':
+ cnt === 3 ? 'Argument found' :
+'Arguments found');

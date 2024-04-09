b44c7f6d4cce9bb2c562f4fd6ef8a915a4d71ab7
hashkitti001 <iJeddy5.0@gmail.com>
2024-04-09 07:43:34 +0100

    adjusted 2-arguments to conform to the semistandard linter

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const cnt = process.argv.length;
-console.log(cnt <= 2 ? 'No argument':
- cnt === 3 ? 'Argument found' :
-'Arguments found');
+console.log(cnt <= 2
+  ? 'No argument'
+  : cnt === 3
+    ? 'Argument found'
+    : 'Arguments found');

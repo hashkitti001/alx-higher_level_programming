6a9837b1bc8277f6b75a5b1934129fc11422b769
hashkitti001 <iJeddy5.0@gmail.com>
2024-04-25 20:04:56 +0100

    add task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# A script that sends a request to a URL passed as an argument, and displays the body of the response
- curl -sI $1 | grep "Content-Length" | cut -d ' ' -f2
+# A script that write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+ curl -sL $1

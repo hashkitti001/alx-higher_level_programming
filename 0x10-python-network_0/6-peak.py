e9afa3f683677c60f48c1d53358e7a9a46040cdc
hashkitti001 <iJeddy5.0@gmail.com>
2024-04-25 20:47:19 +0100

    task 6 but with binary search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 # A function that finds a peak in a list of unsorted integers.
 def find_peak(list_of_integers):
-    arrlen = len(list_of_integers)
-    #Sort the list with insertion sort 
-    for i in range(1, arrlen):
-        curr = list_of_integers[i]
-        while (list_of_integers[i - 1] > curr and i > 0):
-            list_of_integers[i], list_of_integers[i - 1] = list_of_integers[i - 1], list_of_integers[i]
-            i -= 1
-    #Return the last one since the last one in sorted lexicographic order is the peak
-    result = None if arrlen == 0 else list_of_integers[arrlen - 1]
-    return result
+    if not list_of_integers:
+        return None
+    
+    low = 0
+    high = len(list_of_integers) - 1
+    
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+    
+    return list_of_integers[low]
+

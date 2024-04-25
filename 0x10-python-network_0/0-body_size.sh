0f64d28adc9476bd636ae17122aa783fe5f677ec
hashkitti001 <iJeddy5.0@gmail.com>
2024-04-25 20:08:00 +0100

    ....

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 # A script that sends a request to a URL and displays the size of the body of the response
  curl -sI $1 | grep "Content-Length" | cut -d ' ' -f2
+ 
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 # A script that write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
  curl -sL $1
+ 

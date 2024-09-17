6e2cb6fc64de496593db4324cd6b6eba3cb74679
hashkitti001 <iJeddy5.0@gmail.com>
2024-09-17 08:00:58 +0100

    Add readme and first task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const cb = (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  if (data) {
-    console.log(data.toString());
-  }
+ console.log(err || data);
 };
 fs.readFile(process.argv[2], 'utf-8', cb);
